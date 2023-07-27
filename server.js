865cbe5b5a0bc5a90864756cc8f2a0bd0b8f7a2f
Ifeanyi Ani <ifeanyiani155@gmail.com>
2023-07-27 23:02:12 +0100

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000;
 
 // const CON_STR = 'mongodb://127.0.0.1:27017/tumblr-app'
 
-mongoose.connect(`${process.env.DATABASE}`, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.connection.on('open', () => console.log('server is connected'))
 mongoose.connection.on('error', (error) => console.log(error))
 
