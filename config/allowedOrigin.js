e05fbdc8c0adf424f8ebcecfd3237b5abf891fe7
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-04-11 13:13:25 +0100

    update

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
+  "https://tumblr-app-clone.vercel.app/",
   "http://localhost:5173",
   "http://127.0.0.1:4000",
-  "https://tumblr-app-clone.vercel.app/",
 ];
 module.exports = allowedOrigin;
