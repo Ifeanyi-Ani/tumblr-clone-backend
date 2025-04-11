bbc5087365f603fc6a55d581fb9cae48af253c15
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-04-11 13:00:47 +0100

    fix: cors error

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
   "http://localhost:5173",
   "http://127.0.0.1:4000",
-  "https://tumblr-blog.vercel.app",
+  "https://tumblr-app-clone.vercel.app/",
 ];
 module.exports = allowedOrigin;
